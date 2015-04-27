1fd59868d26f5e8cfa4122b4cf39430650a7c642
Mikael Nortikyan <nortikyan99@gmail.com>
2015-04-27 01:28:01 -0700

    Milestone 8
    I added a Google Font, Added more CSS to it, and fixed a typo in my index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 	<div class="wrap">
 		<div class="task-list">
 			<ul>
-			 <<?php require("includes/connect.php"); ?>
+			 <?php require("includes/connect.php"); ?>
 			 </ul>
 		</div>
 		<form class="add-new-task" autocomplete="off">
